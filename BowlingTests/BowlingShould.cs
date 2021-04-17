e756ff0a1a4c7ce26e1d54922baa7c2f7fe4541b
Sebastian Geroli <sgeroli@widowgames.com>
2021-04-17 10:27:10 -0300

    init commit

---
+++ BowlingTests/BowlingShould.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingKata;
+namespace BowlingTests
+{
+    [TestClass]
+    public class BowlingShould
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
