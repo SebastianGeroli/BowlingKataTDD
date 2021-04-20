ee9a0e4f8bd7e2a7d3745fd02b9c0bf99700a3d1
Sebastian Geroli <sebastiangeroli@outlook.com>
2021-04-19 21:35:45 -0300

    Update Program.cs
    Co-Authored-By: artolajp <10684456+artolajp@users.noreply.github.com>
    Co-Authored-By: Sebastian Geroli <sebastiangeroli@outlook.com>
    Co-Authored-By: Cristian <cristianriosv@gmail.com>

--- BowlingKata/Program.cs
+++ BowlingKata/Program.cs
@@ -8,10 +8,19 @@ namespace BowlingKata
         {
             Bowling bowling = new Bowling();
             Console.WriteLine($"Realiza tu jugada!(numeros de 0 a 10)");
-            for(;;){
+            for (int i = 0; i < bowling.Turnos.Length;)
+            {
+                if (i >= 9)
+                {
+                    if (!bowling.Turnos[i-1].esSpare && !bowling.Turnos[i-1].esStrike) break;
+                }
                 bowling.RealizarTirada(int.Parse(Console.ReadLine()));
                 Console.WriteLine($"Tu puntaje Actual es: {bowling.PuntuacionActual}");
+
+                i = bowling.TurnoActual;
             }
+            Console.WriteLine($"Tu puntaje Final es: {bowling.PuntuacionActual}");
+
         }
     }
     public class Bowling
@@ -19,32 +28,41 @@ namespace BowlingKata
         private int cantidadPinosDelTurno = 10;
         private const int cantidadMaxTiradasPorTurno = 2;
         Turno[] turnos = new Turno[12];
+        public Turno[] Turnos => turnos;
         public int TurnoActual = 0;
 
-        public int PuntuacionActual { get{
-            int suma = 0;
-                for(int i = 0 ; i< Turno.turnosMaximos ; ++i){
-                if(turnos[i] != null){                    
-                    suma += turnos[i].cantidadDePinosTirados ;
+        public int PuntuacionActual
+        {
+            get
+            {
+                int suma = 0;
+                for (int i = 0; i < Turno.turnosMaximos; ++i)
+                {
+                    if (turnos[i] != null)
+                    {
+                        suma += turnos[i].cantidadDePinosTirados;
 
-                    if(turnos[i].esSpare && i< turnos.Length-1 && turnos[i+1] != null){
-                        suma+= turnos[i+1].cantidadDePinosTirada1;
-                    }
-
-                    if(turnos[i].esStrike && i< turnos.Length-1 && turnos[i+1] != null){
-                        suma+= turnos[i+1].cantidadDePinosTirados;
-
-                        if(turnos[i+1].esStrike && i< turnos.Length-2 && turnos[i+2] != null){
-                            suma+= turnos[i+2].cantidadDePinosTirada1;
+                        if (turnos[i].esSpare && i < turnos.Length - 1 && turnos[i + 1] != null)
+                        {
+                            suma += turnos[i + 1].cantidadDePinosTirada1;
                         }
 
-                        
-                    }
+                        if (turnos[i].esStrike && i < turnos.Length - 1 && turnos[i + 1] != null)
+                        {
+                            suma += turnos[i + 1].cantidadDePinosTirados;
 
+                            if (turnos[i + 1].esStrike && i < turnos.Length - 2 && turnos[i + 2] != null)
+                            {
+                                suma += turnos[i + 2].cantidadDePinosTirada1;
+                            }
+
+
+                        }
+
+                    }
                 }
+                return suma;
             }
-            return suma;
-            } 
         }
 
         public bool ValidarPinos(int pinos)
@@ -63,16 +81,19 @@ namespace BowlingKata
 
         public void RealizarTirada(int pinos)
         {
-            if(TurnoActual == 0 && turnos[0] == null){
-                turnos[0]= new Turno();
+            if (TurnoActual == 0 && turnos[0] == null)
+            {
+                turnos[0] = new Turno();
             }
 
             turnos[TurnoActual].cantidadTiradasActuales++;
 
-            if(turnos[TurnoActual].cantidadTiradasActuales==1){
+            if (turnos[TurnoActual].cantidadTiradasActuales == 1)
+            {
                 turnos[TurnoActual].cantidadDePinosTirada1 = TirarPinos(pinos);
             }
-            else{
+            else
+            {
                 turnos[TurnoActual].cantidadDePinosTirada2 = TirarPinos(pinos);
             }
 
@@ -85,7 +106,7 @@ namespace BowlingKata
         {
             cantidadPinosDelTurno = 10;
             TurnoActual++;
-            if(TurnoActual >= turnos.Length) return;
+            if (TurnoActual >= turnos.Length) return;
             turnos[TurnoActual] = new Turno();
         }
 
@@ -108,9 +129,9 @@ namespace BowlingKata
         public const int cantidadMaxTiradasPorTurno = 2;
         public int cantidadTiradasActuales = 0;
         public bool habilitado = true;
-        public bool esSpare => cantidadDePinosTirada1 <10 && cantidadDePinosTirados == 10;
+        public bool esSpare => cantidadDePinosTirada1 < 10 && cantidadDePinosTirados == 10;
         public bool esStrike => cantidadDePinosTirada1 == 10;
-        public int cantidadDePinosTirados => (cantidadDePinosTirada1+cantidadDePinosTirada2);
+        public int cantidadDePinosTirados => (cantidadDePinosTirada1 + cantidadDePinosTirada2);
         public int cantidadDePinosTirada1;
         public int cantidadDePinosTirada2;
         public static int turnosMaximos = 10;
